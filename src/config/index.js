dde9dccd21cde3e7f2eb401d12713ecaff567b87
shernanm <santiago_hernandez82151@elpoli.edu.co>
2021-09-09 07:46:53 -0500

    fix firebase config

--- src/config/index.js
+++ src/config/index.js
@@ -2,13 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
 const config = {
-  apiKey: 'AIzaSyDqB7U0El-aq0XSUIoVkGex7zadWz6HZIM',
-  authDomain: 'shop-app-d85d3.firebaseapp.com',
-  databaseURL: 'https://shop-app-d85d3.firebaseio.com',
-  projectId: 'shop-app-d85d3',
-  storageBucket: 'shop-app-d85d3.appspot.com',
-  messagingSenderId: '910074333174',
-  appId: '1:910074333174:web:f9abfd96873b0db1'
+  apiKey: "AIzaSyDW7q9Ow0S8K5a8ckEjHUEU_c5ZAAaJJXw",
+  authDomain: "liapp-d6810.firebaseapp.com",
+  databaseURL: "https://liapp-d6810.firebaseio.com",
+  projectId: "liapp-d6810",
+  storageBucket: "liapp-d6810.appspot.com",
+  messagingSenderId: "64381066729",
+  appId: "1:64381066729:web:8636e09940425bddf87f78",
+  measurementId: "G-JD6PPDCGQD"
 };
 
 const init = () => firebase.initializeApp(config);
